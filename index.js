b7340ff9d69b09e06673452def051d2c856fc4ca
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-24 13:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+  return arr.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (arr) => {
+  return arr.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num) => {
+
+};
+
+const fareDoubler = () => {
+  
+};
+
+const fareTripler = () => {
+  
+};
+
+const selectDifferentDrivers = (arr, func) => {
+  
+};
